9672d2e83b97f19d362d9de69b73c90dbbceae0b
KaseyW31 <kasey.wei121@gmail.com>
2024-03-15 11:37:30 -0400

    clean up old files

--- client/src/components/old/IngredientRow.jsx
+++
@@ -1,9 +0,0 @@
-export default function IngredientRow({ ingredient }) {
-    return (
-      <tr>
-        <td>{ingredient.name}</td>
-        <td>{ingredient.qty}</td>
-        <td>{ingredient.unit}</td>
-      </tr>
-    )
-}
